866312bf25d600170b6e32ab45133b78ae10e730
lhotseweb <56969402+lhotseweb@users.noreply.github.com>
2024-11-23 15:37:50 -0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+        <img src="/Syzygy Logo Side Txt 11-5-24.svg" alt="Syzygy Logo" className={styles.logo} />
       </footer>
     </>
   )
